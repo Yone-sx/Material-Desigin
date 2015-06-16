f9b47687f7a92ee960b87264281a302e07447386
Yone <lsxgoole@gmail.com>
2015-06-16 06:01:39 +0200

    design

---
+++ app/src/main/java/com/example/yone/materialdesign/ListFragment.java
@@ -0,0 +1,32 @@
+package com.example.yone.materialdesign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Yone on 2015/6/15.
+ */
+public class ListFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mRecyclerView = (RecyclerView) inflater.inflate(R.layout.list_fragment,container,false);
+        return mRecyclerView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mRecyclerView.getContext()));
+        mRecyclerView.setAdapter(new RecyclerViewAdapter(getActivity()));
+    }
+}
